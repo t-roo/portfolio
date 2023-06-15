bd244b9d9af2361cbb23d576278f6bf0625c26fd
tanakaryo <web.tanakaryo@gmail.com>
2023-06-15 14:15:51 +0900

    add:license sentence

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: branchName,
+  basePath: branchName,
 }
 
 module.exports = nextConfig
